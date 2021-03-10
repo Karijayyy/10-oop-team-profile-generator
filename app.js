ea737b22b99e3274f6c733b374378321b0c639db
Kari Johnson <kari@crownmediamarketing.com>
2021-03-10 12:50:39 -0700

    created 3 functions on app.js for employee input

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const generateHtml = require("./Utils/generatehtml")
 const teamMembers =[]
 
 // create 5 functions
- // create manager, engineer, intern
+ // create manager
 
 function createManager (){
 inquirer.prompt([
@@ -39,5 +39,68 @@ teamMembers.push(tempManager)
 //call 4th function of what we want to do next 
 })
 }
+ // create engineer
+
+function createEngineer (){
+    inquirer.prompt([
+       {
+           type: "input",
+           message: "What's the engineers name?",
+           name: "engineerName",
+       },
+       {
+        type: "input",
+        message: "What's your ID?",
+        name: "engineerId",
+    }, 
+    {
+        type: "input",
+        message: "What's your email?",
+        name: "engineerEmail",
+    },
+    {
+        type: "input",
+        message: "What's your git Hub account?",
+        name: "engineerGitHub",
+    },
+    ])
+    .then(answers => {
+        let tempEngineer = new Engineer (answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGitHub,)
+    teamMembers.push(tempEngineer)
+    //call 4th function of what we want to do next 
+    })
+    }
+
+     // create intern
+     function createIntern (){
+        inquirer.prompt([
+           {
+               type: "input",
+               message: "What's the interns name?",
+               name: "internName",
+           },
+           {
+            type: "input",
+            message: "What's your ID?",
+            name: "internId",
+        }, 
+        {
+            type: "input",
+            message: "What's your email?",
+            name: "internEmail",
+        },
+        {
+            type: "input",
+            message: "What school did the intern attend?",
+            name: "internSchool",
+        },
+        ])
+        .then(answers => {
+            let tempIntern = new Intern (answers.internName, answers.internId, answers.internEmail, answers.internSchool,)
+        teamMembers.push(tempIntern)
+        //call 4th function of what we want to do next 
+        })
+        }
+
 // create ask what they want to do next function. giving an array of choices to pick from. 
  // create function fs write file after their finished 
